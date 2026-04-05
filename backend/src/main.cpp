27e18b3cb3e7a96868b8fba8d8f1cef18e2117a6
Dima <dima_art2006@mail.ru>
2026-04-05 15:51:33 +0300

    Initial project structure

---
+++ backend/src/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout << "Puzzle Adaptive Server v1.0" << std::endl;
+    std::cout << "Starting..." << std::endl;
+
+    // Пока просто заглушка
+    // Далее добавим сервер, базу данных, игру
+
+    return 0;
+}
